0d024e2e288f66bbc7b539be44953d8d284dcfee
Torres1368 <roberth.torres1368@utc.edu.ec>
2025-05-29 16:30:41 -0500

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+// We'll complete this component later
+export function Home() {
+  return (
+    
+    <div>
+        <p>I'm the home page</p>          
+        <button
+            className="button"
+            type="button"
+            onClick={() => user.logout()}
+          >
+            Salir
+          </button></div>
+  );
+}
